77cf0a0bb3fe9fd15fb21d0868b267c2bae087b1
haokiet6a <93976550+haokiet6a@users.noreply.github.com>
2021-12-28 23:31:25 +0700

    Update func.js

--- js/func.js
+++ js/func.js
@@ -2,7 +2,7 @@ var btn1 = document.querySelector('#btn1');
 var toggle = false;
 function chngimg(){
     if (toggle === true)  {
-        document.getElementById('dec1').src  = 'images/smile.png';
+        document.getElementById('dec1').src  = 'images/smile.jpg';
         document.getElementById('btn1').className  = 'far fa-smile-beam';
 
     } else {
